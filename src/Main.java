c5b06d8927cb0e68361ffd6760e9289dc3f8de62
Victoria Bocharova <v.s.bocha@gmail.com>
2024-05-02 12:35:00 +0300

    Написан код обработки исключения файла

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int count = 1;
-        while (true) {
+        int count = 0;
+        while (count>=0) {
             System.out.println("Введите путь к папке: ");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
-            if (!fileExists || isDirectory) {
+            if (fileExists == isDirectory) {
                 System.out.println("Файл не существует или указанный путь является путём к папке, а не к файлу");
-                continue;
+               //continue;
             } else {
                 System.out.println("Путь указан верно");
-            }
-            System.out.println("Это файл номер "+ count++);
-        }
+                count++;
+                System.out.println("Это файл номер "+ count);
+                FileAnalysis.analtsisFile(path);
     }
 }
+    }
+}
+//D:\Test\access.log
